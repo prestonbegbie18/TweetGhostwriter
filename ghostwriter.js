88ffc49bc684201711792f07f420ab4435c32105
Unknown <pbegbie@mail.csuchico.edu>
2017-04-06 08:49:38 -0700

    Finished Lab 4-6-17

--- ghostwriter.js
+++ ghostwriter.js
@@ -3,8 +3,16 @@ function writeTweet()
   var randIndex;
   var sentence;
   var intro = ["Have you ever noticed", "Sometimes I think", "What's the deal with", "How is it that"];
-  var body = ["suits sometimes make the wearer thinner,", " this weather", " video games are basically unfinished these days,", " DLC is too expensive," , "people just love their coffee?"];
-  var end = ["oh well." , "Nuff said."];
+  var body = ["suits sometimes make the wearer thinner.", " this weather is perfect!", " video games are basically unfinished these days.", " DLC is too expensive." , "people just love their coffee?"];
+  var end = ["Oh well." , "'Nuff said."];
+  randIndex = randomUpTo(intro.length - 1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length - 1);
+  sentence = sentence + " " + body[randIndex];
+  randIndex = randomUpTo(end.length - 1);
+  sentence = sentence + " " + end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
+
 }
 
 //Generates a random whole number between 0 and max (inclusive)
